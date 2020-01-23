14e35b97fdc9e64cf1807c6d9867ef0a7e94105d
baberarjumand <baber.arjumand@gmail.com>
2020-01-23 13:59:16 +0300

    completed exercise 3.5, promises p2, simulated timeouts and added waiting spinners

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { FormsModule } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
@@ -65,7 +66,8 @@ import { LeaderService } from './services/leader.service';
     MatCheckboxModule,
     MatSelectModule,
     MatSlideToggleModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatProgressSpinnerModule
   ],
   providers: [
     DishService,
