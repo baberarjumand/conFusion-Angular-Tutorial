f45b5f1ca5483a5b8f021b03a234f0f59ba6476c
baberarjumand <baber.arjumand@gmail.com>
2020-01-25 11:50:01 +0300

    completed exercise 3.6, animations p1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -6,6 +6,7 @@ import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { switchMap } from 'rxjs/operators';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 // const DISH = {
 //   id: '0',
@@ -54,7 +55,20 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    trigger('visibility', [
+        state('shown', style({
+            transform: 'scale(1.0)',
+            opacity: 1
+        })),
+        state('hidden', style({
+            transform: 'scale(0.5)',
+            opacity: 0
+        })),
+        transition('* => *', animate('0.5s ease-in-out'))
+    ])
+  ]
 })
 export class DishdetailComponent implements OnInit {
 
@@ -65,6 +79,7 @@ export class DishdetailComponent implements OnInit {
     next: string;
     errMess: string;
     dishcopy: Dish;
+    visibility = 'shown';
 
     commentForm: FormGroup;
     comment: Comment;
@@ -106,10 +121,9 @@ export class DishdetailComponent implements OnInit {
 
       // use route params to enable prev/next dish
       this.dishService.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
-      this.route.params
-        .pipe(switchMap((params: Params) => this.dishService.getDish(params['id'])))
-        .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); },
-          errmess => this.errMess = <any>errmess );
+      this.route.params.pipe(switchMap((params: Params) => { this.visibility = 'hidden'; return this.dishService.getDish(+params['id']); }))
+        .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); this.visibility = 'shown'; },
+          errmess => this.errMess = <any>errmess);
       this.createCommentForm();
     }
 
